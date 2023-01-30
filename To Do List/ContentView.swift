69a31a15ec5684c31d054b1452dc6e83a4dfc3d7
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-01-30 10:07:18 -0600

    created initial To-Do list

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,21 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
+    @State private var toDoItems =
+    
+    [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+     
+     ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+     
+     ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(things, id: \.self) { thing in
-                    Text(thing)
+                ForEach(toDoItems) { item in
+                    Text(item.description)
                 }
                 .onMove {indices, newOffset in
-                    things.move(fromOffsets: indices, toOffset: newOffset)
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
                 }
                 .onDelete { IndexSet in
-                    things.remove(atOffsets: IndexSet)
+                    toDoItems.remove(atOffsets: IndexSet)
                 }
             }
-            .navigationBarTitle("Things", displayMode: .inline)
+            .navigationBarTitle("To Do List", displayMode: .inline)
             .navigationBarItems(leading: EditButton())
         }
     }
