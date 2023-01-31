439351fdcd983d7d4f869cf2d58c0489faa11947
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-01-31 09:01:21 -0600

    created add items views

--- To Do List/AddItemView.swift
+++ To Do List/AddItemView.swift
@@ -7,28 +7,28 @@
 
 import SwiftUI
 
-//struct AddItemView: View {
-//    @ObservableObject var toDoList: To_Do_ListApp
-//    @State private var priority = ""
-//    @State private var description = ""
-//    @State private var dueDate = Date()
-//    @Environment (\.presentationMode) var presentationMode
-//    static let priorties = ["High", "Medium", "Low"]
-//    var body: some View {
-//        NavigationView {
-//            Form {
-//                Picker("Priority", selection: $priority) {
-//                    ForEach(Self.priorties, id: \.self) {_ in
-//                        Text(priority)
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//struct AddItemView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AddItemView(toDoList: To_Do_ListApp())
-//    }
-//}
+struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment (\.presentationMode) var presentationMode
+    static let priorties = ["High", "Medium", "Low"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorties, id: \.self) {_ in
+                        Text(priority)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList: ToDoList())
+    }
+}
